119e93cc077d31e3fddf8da7774f1742e320b71d
shunyi <M157q.tw@gmail.com>
2018-04-15 04:54:03 +0800

    Change BTZ_CHECK_INTERVAL_SECONDS to 15 minutes

--- config.example.py
+++ config.example.py
@@ -3,6 +3,8 @@ TWITTER_CONSUMER_SECRET = ""
 TWITTER_ACCESS_TOKEN_KEY = ""
 TWITTER_ACCESS_TOKEN_SECRET = ""
 
-BTZ_CHECK_INTERVAL_SECONDS = 1
+# Twitter Rate limiting use 15 minutes window
+# <https://developer.twitter.com/en/docs/basics/rate-limiting>
+BTZ_CHECK_INTERVAL_SECONDS = 60*15
 BTZ_LAST_NEWEST_FOLLOWER_ID_FILENAME = "last_newest_follower_id"
 BTZ_BLOCKED_USERS_LOG_FILENAME = "blocked_users"
